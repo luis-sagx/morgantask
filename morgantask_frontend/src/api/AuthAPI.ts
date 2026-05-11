bf00993f1e931a27fccde53701ffb49c2daa493b
Luis Sagnay <sagnayluis99@gmail.com>
2026-05-11 15:39:43 -0500

    feat: add frontend types, API layer, axios and auth hook

---
+++ morgantask_frontend/src/api/AuthAPI.ts
@@ -0,0 +1,54 @@
+import api from "@/lib/axios";
+import { isAxiosError } from "axios";
+import { CheckPasswordForm, UserLoginForm, UserRegistrationForm, userSchema } from "../types";
+
+export async function createAccount(formData: UserRegistrationForm) {
+    try {
+        const url = '/auth/create-account'
+        const { data } = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function authenticateUser(formData: UserLoginForm) {
+    try {
+        const url = '/auth/login'
+        const { data } = await api.post<string>(url, formData)
+        localStorage.setItem('AUTH_TOKEN', data)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function getUser() {
+    try {
+        const { data } = await api('/auth/user')
+        const response = userSchema.safeParse(data)
+        if(response.success) {
+            return response.data
+        }
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+export async function checkPassword(formData: CheckPasswordForm) {
+    try {
+        const url = '/auth/check-password'
+        const { data } = await api.post<string>(url, formData)
+        return data
+    } catch (error) {
+        if(isAxiosError(error) && error.response) {
+            throw new Error(error.response.data.error)
+        }
+    }
+}
