005a3ad808dc4de9e6a760fd4241a48ddb8a46de
Jefferson Yepez <jeffersonyepez621@gmail.com>
2026-05-14 09:15:56 -0500

    ci/cd

--- morgantask_backend/src/__tests__/basic.test.ts
+++ morgantask_backend/src/__tests__/basic.test.ts
@@ -1,5 +1,5 @@
-describe("Prueba básica del backend", () => {
-  test("debe validar que Jest está funcionando", () => {
-    expect(2 + 2).toBe(4);
-  });
-});
+// describe("Prueba básica del backend", () => {
+//   test("debe validar que Jest está funcionando", () => {
+//     expect(2 + 2).toBe(4);
+//   });
+// });
